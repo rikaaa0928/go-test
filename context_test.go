e3f1c4663280789100b55b338c90a85312b631df
rikaaa0928 <wangzhidong1@xiaomi.com>
2022-12-27 15:28:08 +0800

    test

--- context_test.go
+++ context_test.go
@@ -2,6 +2,7 @@ package go_test
 
 import (
 	"context"
+	"github.com/rikaaa0928/rtools/generic"
 	"testing"
 )
 
@@ -12,5 +13,28 @@ func TestContext(t *testing.T) {
 	for i := 0; i < 10; i++ {
 		ctx = context.WithValue(ctx, "key", i)
 		t.Log(ctx.Value("key"))
+	}
+	ctxInner(ctx, t)
+	t.Log(ctx.Value("key"))
+	ctx = context.WithValue(ctx, "pointer", generic.TakeAddr(10))
+	t.Log(*ctx.Value("pointer").(*int))
+	ctxPointerInner(ctx, t)
+	t.Log(*ctx.Value("pointer").(*int))
+}
+
+func ctxInner(ctx context.Context, t *testing.T) {
+	ctx = context.WithValue(ctx, "key", "x")
+	t.Log(ctx.Value("key"))
+}
+
+func ctxPointerInner(ctx context.Context, t *testing.T) {
+	p, ok := ctx.Value("pointer").(*int)
+	if ok {
+		*p = 11
+		t.Log(*ctx.Value("pointer").(*int))
+	}
+	_, ok = ctx.Value("key").(*int)
+	if ok {
+		t.Error("should not ok")
 	}
 }
